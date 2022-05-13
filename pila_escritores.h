d12ecbd7b1b0403871283b270c82b226a644e75c
Seali12 <lisandro.bachur@gmail.com>
2022-05-13 09:44:23 -0300

    att y met pila

--- pila_escritores.h
+++ pila_escritores.h
@@ -1,21 +1,41 @@
 #ifndef PILA_ESCRITORES_H
 #define PILA_ESCRITORES_H
 
-class pila{
+class Pila{
 private:
-    Nodo*ultimo;
-    Escritor escritor_nue;
+    Nodo*ultimo;   
+    Escritor escritor;
+
 public:
     
     //PRE:-
     //POST: construye pila vacia
-    Pilas();
+    Pila();
 
     
     //PRE: recibe un daot valido-pila no esta llena
     //POST: da de alta un nuevo escritor
-    void alta(Escritor escri);
+    void alta(Escritor escritor_alta );
 
+
+    //PRE:Pila no vacia
+    //POST:quita el ultimo 
+    void baja();
+
+
+    //PRE:la pila no puede estar vacia
+    //POST:retorna el ultimo elemento 
+    Escritor consulta();
+
+
+    //PRE:-
+    //POST:devuelve true si esta llena
+    bool llena();
+
+
+    //PRE:-
+    //POST:devuelve true si la pila esta vacia
+    bool vacia();
 };
 
 
