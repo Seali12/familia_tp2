f7e7e6f206140502d647c4ded9b65f60a55b4f8f
Seali12 <lisandro.bachur@gmail.com>
2022-05-13 09:35:05 -0300

    pila.h

--- pila_escritores.h
+++ pila_escritores.h
@@ -2,8 +2,19 @@
 #define PILA_ESCRITORES_H
 
 class pila{
+private:
+    Nodo*ultimo;
+    Escritor escritor_nue;
+public:
+    
+    //PRE:-
+    //POST: construye pila vacia
+    Pilas();
 
-
+    
+    //PRE: recibe un daot valido-pila no esta llena
+    //POST: da de alta un nuevo escritor
+    void alta(Escritor escri);
 
 };
 
