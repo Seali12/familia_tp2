a27f99082a1e94c5be7cbe4e5eaa70abf5834176
Seali12 <lisandro.bachur@gmail.com>
2022-05-15 21:08:02 -0300

    cree el const y dest

--- pila_escritores.cpp
+++ pila_escritores.cpp
@@ -4,12 +4,10 @@
 
 Pila::Pila(){
     ultimo = nullptr;
-    //llamar constructor de ESCRITOR
-
 }
 
 bool Pila::vacia(){
-    return(ultimo == 0);
+    return(ultimo == nullptr);
 }
 
 Escritor Pila::consulta(){
@@ -19,19 +17,20 @@ Escritor Pila::consulta(){
 
 void Pila::alta(Escritor escritor_alta){
     //hacerlo con los demas
+    Nodo<Escritor>*nuevo= new Nodo<Escritor>(escritor_alta);
+    nuevo->cambiar_siguiente(ultimo);
+    ultimo=nuevo;
 
 }
 void Pila::baja(){
     Nodo<Escritor>*borrar=ultimo;
-    ultimo=ultimo->obtener_siguiente();//es obtener siguiente o objeto????
+    ultimo=ultimo->obtener_siguiente();
+    delete ultimo;
 }
 
 
-//REVISAR DESTRUCTOR
-Pila::~Pila(){
+void Pila::liberar_Pila(){
     while( ! vacia() ){
         baja();
     }
 }
-
-
